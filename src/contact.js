e3cf7f4f272f6344d5e34fcd220ad0403206fc55
The Alien Lord <79852087+Crawkidile@users.noreply.github.com>
2022-03-14 11:56:24 -0400

    adding JSDoc values to contact

--- src/contact.js
+++ src/contact.js
@@ -1,25 +1,43 @@
 class Contact {
-  constructor() {
+  /**
+   * @param {string} namePrefix 
+   * @param {string} givenName
+   * @param {string} middleName
+   * @param {string} familyName
+   * @param {string} nickname
+   * @param {Date} birthday
+   * @param {[string]} emailAddresses
+   * @param {[string]} phoneNumbers
+   * @param {[string]} postalAddresses
+   * @param {[string]} socialProfiles
+   * @param {string} note
+   * @param {[string]} urlAddresses
+   * @param {[any]} dates
+   * @param {string} organizationName
+   * @param {string} departmentName
+   * @param {string} jobTitle
+   */
+  constructor(namePrefix, givenName, middleName, familyName, nickname, birthday, emailAddresses, phoneNumbers, postalAddresses, socialProfiles, note, urlAddresses, dates, organizationName, departmentName, jobTitle) {
     Object.defineProperty(this, "identifier", {
       value: ""
     });
-    this.namePrefix = "";
-    this.givenName = "";
-    this.middleName = "";
-    this.familyName = "";
-    this.nickname = "";
-    this.birthday = null;
+    this.namePrefix = namePrefix;
+    this.givenName = givenName;
+    this.middleName = middleName;
+    this.familyName = familyName;
+    this.nickname = nickname;
+    this.birthday = birthday;
     this.image = null;
-    this.emailAddresses = [];
-    this.phoneNumbers = [];
-    this.postalAddresses = [];
-    this.socialProfiles = [];
-    this.note = "";
-    this.urlAddresses = [];
-    this.dates = [];
-    this.organizationName = "";
-    this.departmentName = "";
-    this.jobTitle = "";
+    this.emailAddresses = emailAddresses;
+    this.phoneNumbers = phoneNumbers;
+    this.postalAddresses = postalAddresses;
+    this.socialProfiles = socialProfiles;
+    this.note = note;
+    this.urlAddresses = urlAddresses;
+    this.dates = dates;
+    this.organizationName = organizationName;
+    this.departmentName = departmentName;
+    this.jobTitle = jobTitle;
     
     let availableProperties = [];
     for (item of [
